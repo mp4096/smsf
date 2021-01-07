e3f3e9e73dbf98c931e5d924bcfb8e6520b66744
Mikhail Pak <mikhail.pak@tum.de>
2021-01-07 22:23:45 +0100

    Reorganize trait impls for classic stack

--- smsflib/src/stack/implementations/classic/basic_stack_operations_impl.rs
+++ smsflib/src/stack/implementations/classic/basic_stack_operations_impl.rs
@@ -1,7 +1,6 @@
 use super::types::ClassicStack;
 use crate::error::Error as SmsfError;
 use crate::stack::BasicStackOperations;
-use crate::stack::InPlaceFnApplication;
 
 impl<T: num_traits::Zero + Clone> BasicStackOperations for ClassicStack<T> {
     /// # Note
@@ -239,91 +238,6 @@ impl<T: num_traits::Zero + Clone> BasicStackOperations for ClassicStack<T> {
         self.y = zero();
         self.z = zero();
         self.t = zero();
-        Ok(())
-    }
-}
-
-impl<T: Clone> InPlaceFnApplication for ClassicStack<T> {
-    /// # Note
-    /// All functions always return [Ok], since the stack has fixed size.
-
-    type Elem = T;
-    /// Appy a unary operation to the X register in-place.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use smsflib::prelude::*;
-    ///
-    /// let mut stack = ClassicStack::<u32>::new(1, 2, 3, 4);
-    /// let res = stack.unary_fn_in_place(|x: &mut u32| {*x += 10; } );
-    ///
-    /// assert_eq!(res, Ok(()));
-    ///
-    /// assert_eq!(*stack.x(), 11);
-    /// assert_eq!(*stack.y(), 2);
-    /// assert_eq!(*stack.z(), 3);
-    /// assert_eq!(*stack.t(), 4);
-    /// ```
-    fn unary_fn_in_place<U: FnOnce(&mut Self::Elem)>(
-        &mut self,
-        unary_fn: U,
-    ) -> Result<(), SmsfError> {
-        unary_fn(&mut self.x);
-        Ok(())
-    }
-
-    /// Appy a binary operation to the X and Y registers, consuming them.
-    /// Leave the result in X, shift other registers down, cloning the T register.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use smsflib::prelude::*;
-    ///
-    /// let mut stack = ClassicStack::<u32>::new(100, 10, 3, 4);
-    /// let res = stack.binary_fn_in_place_first_arg(|x: &mut u32, y: &u32| {*x -= y; } );
-    ///
-    /// assert_eq!(res, Ok(()));
-    ///
-    /// assert_eq!(*stack.x(), 90);
-    /// assert_eq!(*stack.y(), 3);
-    /// assert_eq!(*stack.z(), 4);
-    /// assert_eq!(*stack.t(), 4);
-    /// ```
-    fn binary_fn_in_place_first_arg<U: FnOnce(&mut Self::Elem, &Self::Elem)>(
-        &mut self,
-        binary_fn: U,
-    ) -> Result<(), SmsfError> {
-        binary_fn(&mut self.x, &self.y);
-        self.y = std::mem::replace(&mut self.z, self.t.clone());
-        Ok(())
-    }
-
-    /// Appy a binary operation to the X and Y registers, consuming them.
-    /// Leave the result in X, shift other registers down, cloning the T register.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use smsflib::prelude::*;
-    ///
-    /// let mut stack = ClassicStack::<u32>::new(10, 100, 3, 4);
-    /// let res = stack.binary_fn_in_place_second_arg(|x: &u32, y: &mut u32| {*y -= x; } );
-    ///
-    /// assert_eq!(res, Ok(()));
-    ///
-    /// assert_eq!(*stack.x(), 90);
-    /// assert_eq!(*stack.y(), 3);
-    /// assert_eq!(*stack.z(), 4);
-    /// assert_eq!(*stack.t(), 4);
-    /// ```
-    fn binary_fn_in_place_second_arg<U: FnOnce(&Self::Elem, &mut Self::Elem)>(
-        &mut self,
-        binary_fn: U,
-    ) -> Result<(), SmsfError> {
-        binary_fn(&self.x, &mut self.y);
-        self.x = std::mem::replace(&mut self.y, std::mem::replace(&mut self.z, self.t.clone()));
         Ok(())
     }
 }
--- smsflib/src/stack/implementations/classic/mod.rs
+++ smsflib/src/stack/implementations/classic/mod.rs
@@ -1,7 +1,9 @@
 //! Classic HP-like stack with a fixed size of four registers: X, Y, Z, T
 
 /// Implementation of the [BasicStackOperations](crate::stack::BasicStackOperations) trait
-mod basic_ops;
+mod basic_stack_operations_impl;
+/// Implementation of the [InPlaceFnApplication](crate::stack::InPlaceFnApplication) trait
+mod in_place_fn_application_impl;
 /// Data type definitions
 mod types;
 
